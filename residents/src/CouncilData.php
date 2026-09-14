154a3352fe90670ada783c558a1d1c10751cbfd4
DigiStaff <digistaff.team@gmail.com>
2026-09-14 21:56:53 +0300

    Совет: добавлен протокол встречи 14.09.2026

--- residents/src/CouncilData.php
+++ residents/src/CouncilData.php
@@ -76,6 +76,7 @@ final class CouncilData
     public static function protocols(): array
     {
         return [
+            ['title' => 'Протокол встречи Совета 14.09.2026', 'href' => 'https://docs.google.com/document/d/10Sw-qPyvz7OpvJ6U754k7hjvWOTKzfj7jOyiCnlioj8/edit?usp=sharing'],
             ['title' => 'Протокол встречи Совета 07.09.2026', 'href' => 'https://docs.google.com/document/d/1PecerQK3pH13BDITJBHOPW27Uub-v5rJWGPIFHfRQm8/edit?usp=sharing'],
             ['title' => 'Протокол встречи Совета 31.08.2026', 'href' => 'https://docs.google.com/document/d/19jvEIG5hRWNoUV_xorT8Zh0r_0grnO8vp-tFbws1Osk/edit?usp=drive_link'],
             ['title' => 'Протокол встречи Совета 17.08.2026', 'href' => 'https://docs.google.com/document/d/1mMVSirvpxGw7XyOeKqySSsiVJz6xIjzjwf06QHtS2sQ/edit?usp=drive_link'],
